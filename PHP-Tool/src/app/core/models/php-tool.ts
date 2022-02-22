19f2f6dea15882cf7a7856b12d00b9fc38421a10
Nils Bezjak <nils.bezjak@gmail.com>
2022-02-22 10:08:42 +0100

    feat: adding model and service for user from LogIn

--- PHP-Tool/src/app/core/models/php-tool.ts
+++ PHP-Tool/src/app/core/models/php-tool.ts
@@ -1,11 +1,43 @@
 import {v4 as uuidv4} from 'uuid';
 
+//Array with all users
+//one user is stored as UserData
+export interface UsersProps {
+    users: UserDataProps[];
+}
+//Props for one user
 export interface UserDataProps {
     name: string;
     password: string;
     uuid?: string;
 }
+//class for all users
+export class Users {
+    private _users: UserData[];
 
+    constructor(props?: UsersProps){
+        if(props === undefined){
+            this._users = [];
+            return;
+        }
+        this._users = props.users.map(u => new UserData(u));
+    }
+
+    get users(): UserData[]{
+        return this._users;
+    }
+
+    set users(newUsers: UserData[]) {
+        this._users = newUsers;
+    }
+
+    getProps(): UsersProps {
+        return {
+            users: this.users.map(u => u.getProps())
+        };
+    }
+}
+//class for a single user
 export class UserData {
     private _uuid: string;
     private _password: string;
--- PHP-Tool/src/app/core/services/php-tool.service.ts
+++ PHP-Tool/src/app/core/services/php-tool.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { UserData, UserDataProps, Users } from '../models/php-tool';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhpToolService {
+  private _users: Users;
 
-  constructor() { }
+  constructor() { 
+    this._users = new Users
+  }
+
+  addUser(props: UserDataProps): UserData {
+    let user = new UserData(props);
+
+    this._users.users.push(user)
+    return user;
+  }
 }
