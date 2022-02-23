1150f684e7a0938ad85b319dcaab6169f1cb554b
Nils Bezjak <21.bezjak.nils@kas-bc.de>
2022-02-23 17:40:18 +0100

    feat: basic layout of the page

--- PHP-Tool/src/app/feature/php-tool/acc-management-view/acc-management-view.component.ts
+++ PHP-Tool/src/app/feature/php-tool/acc-management-view/acc-management-view.component.ts
@@ -5,11 +5,33 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './acc-management-view.component.html',
   styleUrls: ['./acc-management-view.component.scss']
 })
-export class AccManagementViewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class AccManagementViewComponent {
+  displayedColumns = ['name', 'seriennummer', 'praxis', 'version'];
+  dataSource = ELEMENT_DATA;
 }
+
+export interface PeriodicElement {
+  name: string;
+  version: string;
+  sn: number;
+  praxis: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {version: '234-2349-02834', name: 'Hanz Müller', praxis: 'ZAHNwehLOS', sn: 239847239239487},
+  {version: '234-2349-02834', name: 'Hanz Müller', praxis: 'ZAHNwehLOS', sn: 239847239239487},
+  {version: '234-2349-02834', name: 'Hanz Müller', praxis: 'ZAHNwehLOS', sn: 239847239239487},
+  {version: '234-2349-02834', name: 'Hanz Müller', praxis: 'ZAHNwehLOS', sn: 239847239239487},
+  {version: '234-2349-02834', name: 'Hanz Müller', praxis: 'ZAHNwehLOS', sn: 239847239239487},
+  {version: '234-2349-02834', name: 'Hanz Müller', praxis: 'ZAHNwehLOS', sn: 239847239239487},
+  {version: '234-2349-02834', name: 'Hanz Müller', praxis: 'ZAHNwehLOS', sn: 239847239239487},
+  {version: '234-2349-02834', name: 'Hanz Müller', praxis: 'ZAHNwehLOS', sn: 239847239239487},
+  {version: '234-2349-02834', name: 'Hanz Müller', praxis: 'ZAHNwehLOS', sn: 239847239239487},
+  {version: '234-2349-02834', name: 'Hanz Müller', praxis: 'ZAHNwehLOS', sn: 239847239239487},
+  {version: '234-2349-02834', name: 'Hanz Müller', praxis: 'ZAHNwehLOS', sn: 239847239239487},
+  {version: '234-2349-02834', name: 'Hanz rasoteh', praxis: 'ZAHNwehLOS', sn: 239847239239487},
+  {version: '234-2349-02834', name: 'Hanz rasoteh', praxis: 'ZAHNwehLOS', sn: 239847239239487},
+  {version: '234-2349-02834', name: 'Hanz rasoteh', praxis: 'ZAHNwehLOS', sn: 239847239239487},
+  {version: '234-2349-02834', name: 'Hanz rasoteh', praxis: 'ZAHNwehLOS', sn: 239847239239487},
+  {version: '234-2349-02834', name: 'Hanz rasoteh', praxis: 'ZAHNwehLOS', sn: 239847239239487},
+];
