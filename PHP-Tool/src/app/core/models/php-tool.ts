e9de44a1d7b4f521bc47f4fbd8b066e90e326223
Nils Bezjak <21.bezjak.nils@kas-bc.de>
2022-03-01 14:50:05 +0100

    feat: adding dev users to model

--- PHP-Tool/src/app/core/models/php-tool.ts
+++ PHP-Tool/src/app/core/models/php-tool.ts
@@ -1,175 +1,279 @@
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 // Array with all users
 // One user is stored as UserData
 export interface UsersProps {
-    users: UserDataProps[];
+  users: UserDataProps[];
 }
 // Props for one user
 export interface UserDataProps {
-    name: string;
-    password: string;
-    uuid?: string;
+  name: string;
+  password: string;
+  uuid?: string;
 }
 // props for all test users
 export interface AllTestUsersProps {
-    testUsers: SingleTestUserProps[];
+  testUsers: SingleTestUserProps[];
 }
 // Props for one Test user
 export interface SingleTestUserProps {
-  uuid?: string; // unique id   
+  uuid?: string; // unique id
   name: string; // name of dentist
-  sn: number; // serialnumber 
+  sn: number; // serialnumber
   praxis: string; // name of praxis
   version: string; // newest version dentist has access to
 }
+// props for all dev users
+export interface AllDevUsersProps {
+  devUsers: SingleDevUserProps[];
+}
+// props for one dev user
+export interface SingleDevUserProps {
+  uuid?: string; // unique id
+  name: string; // name of dentist
+  sn: number; // serialnumber
+  praxis: string; // name of praxis
+  version: string; // newest version dentist has access to
+}
+
+
+
 // class for managing all users that have logged in
 export class Users {
-    private _users: UserData[];
+  private _users: UserData[];
 
-    constructor(props?: UsersProps){
-        if(props === undefined){
-            this._users = [];
-            return;
-        }
-        this._users = props.users.map(u => new UserData(u));
+  constructor(props?: UsersProps) {
+    if (props === undefined) {
+      this._users = [];
+      return;
     }
+    this._users = props.users.map((u) => new UserData(u));
+  }
 
-    get users(): UserData[]{
-        return this._users;
-    }
+  get users(): UserData[] {
+    return this._users;
+  }
 
-    set users(newUsers: UserData[]) {
-        this._users = newUsers;
-    }
+  set users(newUsers: UserData[]) {
+    this._users = newUsers;
+  }
 
-    getProps(): UsersProps {
-        return {
-            users: this.users.map(u => u.getProps())
-        };
-    }
+  getProps(): UsersProps {
+    return {
+      users: this.users.map((u) => u.getProps()),
+    };
+  }
 }
+
 // class for a single user
 export class UserData {
-    private _uuid: string;
-    private _password: string;
-    private _name: string;
+  private _uuid: string;
+  private _password: string;
+  private _name: string;
 
-    constructor(props: UserDataProps){
-        this._name = props.name;
-        this._password = props.password;
-        this._uuid = props.uuid ?? uuidv4();
-    }
+  constructor(props: UserDataProps) {
+    this._name = props.name;
+    this._password = props.password;
+    this._uuid = props.uuid ?? uuidv4();
+  }
 
-    get uuid(): string {
-        return this._uuid;
-    }
+  get uuid(): string {
+    return this._uuid;
+  }
 
-    get name(): string {
-        return this._name;
-    }
-    
-    set name(newName: string) {
-        this._name = newName;
-    }
+  get name(): string {
+    return this._name;
+  }
 
-    get password():string {
-        return this._password;
-    }
+  set name(newName: string) {
+    this._name = newName;
+  }
 
-    set password(newPassword: string) {
-        this._password = newPassword;
-    }
+  get password(): string {
+    return this._password;
+  }
 
-    getProps(): UserDataProps {
-        return {
-            name: this.name,
-            password: this.password,
-            uuid: this.uuid
-        }
-    }
+  set password(newPassword: string) {
+    this._password = newPassword;
+  }
+
+  getProps(): UserDataProps {
+    return {
+      name: this.name,
+      password: this.password,
+      uuid: this.uuid,
+    };
+  }
 }
-// class for all test customers
-export class AllTestCustomers {
-    private _testUsers: SingleTestUser[];
 
-    constructor(props?: AllTestUsersProps){
-        if(props === undefined){
-            this._testUsers = [];
-            return;
-        }
-        this._testUsers = props.testUsers.map(t => new SingleTestUser(t))
-    }
+// class for all test users
+export class AllTestUsers {
+  private _testUsers: SingleTestUser[];
 
-    get testUsers(): SingleTestUser[] {
-        return this._testUsers;
+  constructor(props?: AllTestUsersProps) {
+    if (props === undefined) {
+      this._testUsers = [];
+      return;
     }
+    this._testUsers = props.testUsers.map((t) => new SingleTestUser(t));
+  }
 
-    set testUsers(newTestUsers: SingleTestUser[]) {
-        this._testUsers = newTestUsers;
-    }
+  get testUsers(): SingleTestUser[] {
+    return this._testUsers;
+  }
 
-    getProps(): AllTestUsersProps {
-        return {
-            testUsers: this.testUsers.map(t => t.getProps())
-        };
-    }
+  set testUsers(newTestUsers: SingleTestUser[]) {
+    this._testUsers = newTestUsers;
+  }
+
+  getProps(): AllTestUsersProps {
+    return {
+      testUsers: this.testUsers.map((t) => t.getProps()),
+    };
+  }
 }
+
 // class for a single test user
 export class SingleTestUser {
-    private _uuid: string;
-    private _name: string;
-    private _sn: number;
-    private _praxis: string;
-    private _version: string;
+  private _uuid: string;
+  private _name: string;
+  private _sn: number;
+  private _praxis: string;
+  private _version: string;
 
-    constructor(props: SingleTestUserProps){
-        this._uuid = props.uuid ?? uuidv4();
-        this._name = props.name;
-        this._sn = props.sn;
-        this._praxis = props.praxis;
-        this._version = props.version;
-    }
+  constructor(props: SingleTestUserProps) {
+    this._uuid = props.uuid ?? uuidv4();
+    this._name = props.name;
+    this._sn = props.sn;
+    this._praxis = props.praxis;
+    this._version = props.version;
+  }
 
-    get uuid(): string {
-        return this._uuid;
-    }
+  get uuid(): string {
+    return this._uuid;
+  }
 
-    get name(): string {
-        return this._name;
-    }
-    set name(newName: string) {
-        this._name = newName
-    }
-    
-    get sn(): number {
-        return this._sn;
-    }
-    set sn(newSn: number) {
-        this._sn = newSn;
-    }
+  get name(): string {
+    return this._name;
+  }
+  set name(newName: string) {
+    this._name = newName;
+  }
 
-    get praxis(): string {
-        return this._praxis;
-    }
-    set praxis(newPraxis: string) {
-        this._praxis = newPraxis;
-    }
+  get sn(): number {
+    return this._sn;
+  }
+  set sn(newSn: number) {
+    this._sn = newSn;
+  }
 
-    get version(): string {
-        return this._version;
-    }
-    set version(newVersion: string) {
-        this._version = newVersion;
-    }
+  get praxis(): string {
+    return this._praxis;
+  }
+  set praxis(newPraxis: string) {
+    this._praxis = newPraxis;
+  }
 
-    getProps(): SingleTestUserProps {
-        return {
-            uuid: this._uuid,
-            name: this._name,
-            sn: this._sn,
-            praxis: this._praxis,
-            version: this._version
-        }
+  get version(): string {
+    return this._version;
+  }
+  set version(newVersion: string) {
+    this._version = newVersion;
+  }
+
+  getProps(): SingleTestUserProps {
+    return {
+      uuid: this._uuid,
+      name: this._name,
+      sn: this._sn,
+      praxis: this._praxis,
+      version: this._version,
+    };
+  }
+}
+
+// class for all dev users
+export class AllDevUsers {
+  private _devUsers: SingleDevUser[];
+
+  constructor(props?: AllDevUsersProps) {
+    if (props === undefined) {
+      this._devUsers = [];
+      return;
     }
+    this._devUsers = props.devUsers.map((d) => new SingleDevUser(d));
+  }
+
+  get devUsers(): SingleDevUser[] {
+    return this._devUsers;
+  }
+
+  set devUsers(newDevUsers: SingleDevUser[]) {
+    this._devUsers = newDevUsers;
+  }
+
+  getProps(): AllDevUsersProps {
+    return {
+      devUsers: this.devUsers.map((d) => d.getProps()),
+    };
+  }
+}
+
+// class for one dev user
+export class SingleDevUser {
+  private _uuid: string;
+  private _name: string;
+  private _sn: number;
+  private _praxis: string;
+  private _version: string;
+
+  constructor(props: SingleDevUserProps) {
+    this._uuid = props.uuid ?? uuidv4();
+    this._name = props.name;
+    this._sn = props.sn;
+    this._praxis = props.praxis;
+    this._version = props.version;
+  }
+
+  get uuid(): string {
+    return this._uuid;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+  set name(newName: string) {
+    this._name = newName;
+  }
+
+  get sn(): number {
+    return this._sn;
+  }
+  set sn(newSn: number) {
+    this._sn = newSn;
+  }
+
+  get praxis(): string {
+    return this._praxis;
+  }
+  set praxis(newPraxis: string) {
+    this._praxis = newPraxis;
+  }
+
+  get version(): string {
+    return this._version;
+  }
+  set version(newVersion: string) {
+    this._version = newVersion;
+  }
+
+  getProps(): SingleDevUserProps {
+    return {
+      uuid: this._uuid,
+      name: this._name,
+      sn: this._sn,
+      praxis: this._praxis,
+      version: this._version,
+    };
+  }
 }
