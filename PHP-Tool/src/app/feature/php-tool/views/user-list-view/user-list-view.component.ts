03647da91493d4302bc0a9c45c75fbedfe0c30c3
Nils Bezjak <nils.bezjak@gmail.com>
2022-03-29 12:02:55 +0200

    int: initial commit

---
+++ PHP-Tool/src/app/feature/php-tool/views/user-list-view/user-list-view.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { SingleUser } from 'src/app/core/models/php-tool2';
+
+@Component({
+  selector: 'app-user-list-view',
+  templateUrl: './user-list-view.component.html',
+  styleUrls: ['./user-list-view.component.scss']
+})
+export class UserListViewComponent implements OnInit {
+  @Input() users?: SingleUser[];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ PHP-Tool/src/app/feature/php-tool/views/usergroup-settings-view/usergroup-settings-view.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserGroupSettings } from 'src/app/core/models/php-tool2';
+import { PhpTool2Service } from 'src/app/core/services/php-tool2.service';
+import { DialogSettingsComponent } from '../../Dialogs/dialog-settings/dialog-settings.component';
+
+@Component({
+  selector: 'app-usergroup-settings-view',
+  templateUrl: './usergroup-settings-view.component.html',
+  styleUrls: ['./usergroup-settings-view.component.scss']
+})
+export class UsergroupSettingsViewComponent implements OnInit {
+
+  @Input() settings?: UserGroupSettings;
+  constructor(
+    private phpToolService: PhpTool2Service,
+    public dialog: MatDialog,
+    private snackBar: MatSnackBar
+  ) {}
+
+  openSettingsDialog(flag: string): void {
+    const dialogRef = this.dialog.open<DialogSettingsComponent>(DialogSettingsComponent, {data: undefined});
+
+    dialogRef.afterClosed().subscribe((result) => {
+      this.snackBar.open("Änderungen erfolgreich gespeichert.", "", {duration: 3000})
+    });
+  }
+  ngOnInit(): void {
+  }
+
+  selectFile(): void {
+    let input = document.createElement('input');
+    input.type = 'file';
+    input.onchange = _ => {
+      let file = input.files
+      console.log(file);
+    };
+    input.click();
+  }
+
+}
