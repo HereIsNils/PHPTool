a15e4c2a282d43ac0c9c11a12bd182a4f4e21a5d
Nils Bezjak <nils.bezjak@gmail.com>
2022-03-29 12:03:46 +0200

    ref: updating dialog for new service and model

--- PHP-Tool/src/app/feature/php-tool/Dialogs/dialog-settings/dialog-settings.component.ts
+++ PHP-Tool/src/app/feature/php-tool/Dialogs/dialog-settings/dialog-settings.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { VersionSettingsProps } from 'src/app/core/models/php-tool';
+import { UserGroupSettingsProps } from 'src/app/core/models/php-tool2';
 
 @Component({
   selector: 'app-dialog-settings',
@@ -11,11 +11,11 @@ export class DialogSettingsComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<DialogSettingsComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: VersionSettingsProps) {
+    @Inject(MAT_DIALOG_DATA) public data: UserGroupSettingsProps) {
     if (data === undefined) {
       this.data = {
         path: "",
-        maxDownloads: 0
+        limit: NaN
       }
     }
   }
