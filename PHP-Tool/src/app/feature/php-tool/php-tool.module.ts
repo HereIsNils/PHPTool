870b6c0addf2047fdf5733d4436454e2b7a3a7ca
Nils Bezjak <nils.bezjak@gmail.com>
2022-03-30 09:54:56 +0200

    ref: removed old files

--- PHP-Tool/src/app/feature/php-tool/php-tool.module.ts
+++ PHP-Tool/src/app/feature/php-tool/php-tool.module.ts
@@ -2,11 +2,8 @@ import { DragDropModule } from "@angular/cdk/drag-drop";
 import { NgModule } from "@angular/core";
 import { CoreModule } from "src/app/core/core.module";
 import { PhpToolComponent } from './php-tool.component';
-import { AccManagementViewComponent } from './acc-management-view/acc-management-view.component';
 import { DialogLoginComponent } from './Dialogs/dialog-login/dialog-login.component';
 import { DialogAddUserComponent } from './Dialogs/dialog-add-user/dialog-add-user.component';
-import { TestuserTableComponent } from './acc-management-view/testuser-table/testuser-table.component';
-import { DevuserTableComponent } from './acc-management-view/devuser-table/devuser-table.component';
 import { DialogSettingsComponent } from './Dialogs/dialog-settings/dialog-settings.component';
 import { UsergroupViewComponent } from './usergroup-view/usergroup-view.component';
 import { DialogCreateUsergroupComponent } from './Dialogs/dialog-create-usergroup/dialog-create-usergroup.component';
@@ -17,11 +14,8 @@ import { TableViewComponent } from './views/user-list-view/table-view/table-view
 @NgModule({
     declarations: [
     PhpToolComponent,
-    AccManagementViewComponent,
     DialogLoginComponent,
     DialogAddUserComponent,
-    TestuserTableComponent,
-    DevuserTableComponent,
     DialogSettingsComponent,
     UsergroupViewComponent,
     DialogCreateUsergroupComponent,
