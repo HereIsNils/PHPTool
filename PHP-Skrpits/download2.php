a48f05ebe029f1859ee6da191f7e82acc36b2b74
Nils Bezjak <nils.bezjak@gmail.com>
2022-04-11 10:19:49 +0200

    init: initial commit

--- PHP-Skrpits/download2.php
+++ PHP-Skrpits/download2.php
@@ -7,15 +7,15 @@
 // Updatedatei zurückgeben
 // ggf. Downloads der UserGroup erhöhen
 
-// our root dir & log file
+// our root dir, database dir & log file
 //$rootdir = "/55454_lp_campaign_now/wordpress/connectbase"; 
 $rootdir = $_SERVER["DOCUMENT_ROOT"];
 $rootdir = $rootdir."/connectbase";
+$databasedir = $rootdir."/database";
 $logfile = $rootdir."/logs/".date("Y-m-d").".txt";
 
 // hide notices
 @ini_set('error_reporting', E_ALL & ~ E_NOTICE);
-
 
 //- turn off compression on the server
 //@apache_setenv('no-gzip', 1);
