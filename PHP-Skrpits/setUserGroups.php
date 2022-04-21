71cf186bea00746e30f83dee53e2cd40b1c3b7c3
Nils Bezjak <nils.bezjak@gmail.com>
2022-04-21 15:22:41 +0200

    int: adding path variables and handling post request

--- PHP-Skrpits/setUserGroups.php
+++ PHP-Skrpits/setUserGroups.php
@@ -1,6 +1,15 @@
 <?php
 
-// Diese Datei erstellt die JSON Datei mit allen Usergruppen
+// this script writes the json object sent by the website to the database
 
+// our root dir, database dir & log file
+//$rootdir = "/55454_lp_campaign_now/wordpress/connectbase"; 
+$rootdir = $_SERVER["DOCUMENT_ROOT"];
+$rootdir = $rootdir."/connectbase";
+$databasedir = $rootdir."/database";
+$logfile = $rootdir."/logs/".date("Y-m-d").".txt";
+
+// takes the json object sent from the website 
+$obj = json_decode($_POST["userGroups"], flase);
 
 ?>
