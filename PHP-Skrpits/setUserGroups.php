161a401110746a5b82b8b650badef54d0a89d657
Nils Bezjak <nils.bezjak@gmail.com>
2022-04-22 13:42:07 +0200

    feat: errorhandling for creating or overwriting the file

--- PHP-Skrpits/setUserGroups.php
+++ PHP-Skrpits/setUserGroups.php
@@ -12,7 +12,11 @@ $logfile = $rootdir."/logs/".date("Y-m-d").".txt";
 // takes the json object sent from the website 
 $obj = json_decode($_POST["userGroups"], flase);
 
-// creates or overrides file with data from the POST request
-$file = fopen($databasedir . "userData.json", "w+", false, $obj);
+try {
+    // creates or overrides file with data from the POST request
+    $file = fopen($databasedir . "userData.json", "w+", false, $obj);
+} catch(Exception $e) {
+    echo $e->getMessage();
+}
 
 ?>
