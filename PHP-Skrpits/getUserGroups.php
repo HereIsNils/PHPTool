1d125da59fedc13682d9079b6899b82b0941a629
Nils Bezjak <nils.bezjak@gmail.com>
2022-04-27 07:47:47 +0200

    feat: returning json file as object

---
+++ PHP-Skrpits/getUserGroups.php
@@ -0,0 +1,18 @@
+<?php
+
+// this script returns the json file as an object
+
+// our root dir, database dir & log file
+//$rootdir = "/55454_lp_campaign_now/wordpress/connectbase"; 
+$rootdir = $_SERVER["DOCUMENT_ROOT"];
+$rootdir = $rootdir."/connectbase";
+$databasedir = $rootdir."/database";
+$logfile = $rootdir."/logs/".date("Y-m-d").".txt";
+
+// takes the json object sent from the website 
+$obj = json_decode($databasedir . "/userData.json", flase);
+
+echo  $obj;
+
+exit();
+?>
