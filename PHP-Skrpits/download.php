7862835566cd2ebd2b20d3e496fb8fa7103cfcc6
Nils Bezjak <nils.bezjak@gmail.com>
2022-05-11 14:42:22 +0200

    feat: file gets returned to unit

--- PHP-Skrpits/download.php
+++ PHP-Skrpits/download.php
@@ -4,10 +4,6 @@
 // this can either be a file or nothing if a newer or the same version is already downloaded or/and installed
 
 //TODO
-//UserGruppe finden (wenn nichts gefunden wird -> public version)
-// Versionsnummer überprüfen (wenn < wie downloaded -> public < downloaded -> return 0)
-// wenn nicht public Version max downloads überprüfen
-// Updatedatei zurückgeben
 // ggf. Downloads der UserGroup erhöhen
 
 // our root dir, database dir & log file
@@ -74,11 +70,88 @@ if(!$updateFile){
     $logstring = sprintf("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;noDL:%s".PHP_EOL, $datetime, $userIp, $patcomid, $installedversion, $downloadedversion, $tutype, $tuserial, $tutable, $tufw, $tulic, $version);
     file_put_contents($logfile, $logstring, FILE_APPEND);
     die();
-} else {
-    // prepare and send the file
-
-    
 }
 
-exit()
-?>
+$filename = basename($updateFile);
+if (file_exists($updateFile)) 
+		{
+				header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
+				header("Cache-Control: public"); // needed for internet explorer
+				// set the headers, prevent caching
+				header("Pragma: public");
+				header("Expires: -1");
+				header("Cache-Control: public, must-revalidate, post-check=0, pre-check=0");
+				
+				header("Content-Type: application/octet-stream");
+				header("Content-Transfer-Encoding: Binary");
+				header("Content-Disposition: attachment; filename=".$filename);
+				//header("Content-Length:".filesize($fullfilename));
+				
+				//check if http_range is sent by browser (or download manager)
+				$file_size  = filesize($updateFile);
+				if(isset($_SERVER['HTTP_RANGE']))
+				{
+					list($size_unit, $range_orig) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+					if ($size_unit == 'bytes')
+					{
+						//multiple ranges could be specified at the same time, but for simplicity only serve the first range
+						//http://tools.ietf.org/id/draft-ietf-http-range-retrieval-00.txt
+						list($range, $extra_ranges) = explode(',', $range_orig, 2);
+					}
+					else
+					{
+						$range = '';
+						header('HTTP/1.1 416 Requested Range Not Satisfiable');
+						exit;
+					}
+				}
+				else
+				{
+					$range = '';
+				}
+				
+				//figure out download piece from range (if set)
+				list($seek_start, $seek_end) = explode('-', $range, 2);
+
+				//set start and end based on range (if set), else set defaults
+				//also check for invalid ranges.
+				$seek_end   = (empty($seek_end)) ? ($file_size - 1) : min(abs(intval($seek_end)),($file_size - 1));
+				$seek_start = (empty($seek_start) || $seek_end < abs(intval($seek_start))) ? 0 : max(abs(intval($seek_start)),0);
+			 
+				//Only send partial content header if downloading a piece of the file (IE workaround)
+				if ($seek_start > 0 || $seek_end < ($file_size - 1))
+				{
+					header('HTTP/1.1 206 Partial Content');
+					header('Content-Range: bytes '.$seek_start.'-'.$seek_end.'/'.$file_size);
+					header('Content-Length: '.($seek_end - $seek_start + 1));
+				}
+				else
+				  header("Content-Length:".filesize($updateFile));
+		
+				header('Accept-Ranges: bytes');				
+		
+			$logstring = sprintf("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s".PHP_EOL, $datetime, $userIp, $patcomid, $installedversion, $downloadedversion, $tutype, $tuserial, $tutable, $tufw, $tulic, $filename);
+			file_put_contents($logfile, $logstring, FILE_APPEND); 
+			//readfile($fullfilename); // use loop instead of pure readfile because of large files
+			
+			set_time_limit(0);
+			$file = @fopen($updateFile,"rb");
+			fseek($file, 0);
+			
+			while(!feof($file)) 
+			{
+				print(@fread($file, 1024*8));
+				ob_flush();
+				flush();
+				if (connection_status()!=0) 
+				{
+					@fclose($file);
+					exit;
+				}			
+			}
+			// file save was a success
+			@fclose($file);
+			
+			exit();
+		}
+exit();
